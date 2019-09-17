9b828f8cff2bdec405fb3c2e73329ea29961ec6a
The-afroman <fhijazi00#gmail.com>
2019-09-16 17:11:32 -0700

    modified city.h

--- city.h
+++ city.h
@@ -1,17 +1,24 @@
 
-#ifdef CITY_H
+#ifndef CITY_H
 #define CITY_H
 
 #include <string>
+#include <vector>
 
+using std::string;
 
 class city{
+
+    struct food{
+        string foodItem;
+        string foodDisc;
+    };
 
     public: 
 
         city();
 
-        city(string name, double distance, string * food[], string * foodDesc[]);
+        city(string name, double distance);
 
         ~city();
 
@@ -25,17 +32,12 @@ class city{
 
         string name;
         double distFromParis;
-        string * foodItems[];
-        string * foodDescriptions[];
-
+        std::vector<food> cityFoods;
         void setName(string);
         void setDistanceToParis(double);
-        void setFoodItems(string *[]);
-        void setFoodDesc(string *[]);
+        //void setFoodItems(string *[]);
+        //void setFoodDesc(string *[]);
 
 };
-
-
-
 
 #endif
