0b8a8651b2a7d1f4405ea89bd393cf448094fea5
Robert-GainesIII <gaines.robert.edgar@gmail.com>
2019-09-19 17:49:36 -0700

    progress on populating DB

--- database.h
+++ database.h
@@ -6,11 +6,15 @@
 #include <QtSql/QSqlError>
 #include <QtSql/QSqlQuery>
 #include <QDebug>
+#include <QSqlRecord>
 
 class DbManager
 {
 public:
     DbManager(const QString& path);
+    bool addCity(const QString&,  const QString&, const double &);
+    void printCities();
+    void eraseData();
 private:
     QSqlDatabase m_db;
 };
@@ -28,6 +32,54 @@ DbManager::DbManager(const QString& path)
    {
       qDebug() << "Database: connection ok";
    }
+
+   //USED THIS TO CREATE THE TABLE BUT YOU ONLY NEED TO CREATE IT ONCE SOOOOOOO
+ //QSqlQuery query("CREATE TABLE cities2 (id INTEGER PRIMARY KEY, start TEXT, finish TEXT, distance DOUBLE)");
+ //if(!query.isActive())
+ //    qWarning() << "ERROR: " << query.lastError().text();
+}
+
+bool DbManager::addCity(const QString& start, const QString& finish, const double& distance)
+{
+    bool success = false;
+       // you should check if args are ok first...
+       QSqlQuery query;
+       query.prepare("INSERT INTO cities2 (start, finish, distance) VALUES (:start, :finish, :distance)");
+       query.bindValue(":start", start);
+       query.bindValue(":finish", finish);
+       query.bindValue(":distance", distance);
+       if(query.exec())
+       {
+           success = true;
+       }
+       else
+       {
+            qDebug() << "addPerson error:  "
+                     << query.lastError();
+       }
+
+       return success;
+}
+
+void DbManager::printCities(){
+    QSqlQuery query("SELECT * FROM cities2");
+    int idStart = query.record().indexOf("start");
+    int idFinish = query.record().indexOf("finish");
+    int idDistance = query.record().indexOf("distance");
+    while (query.next())
+    {
+       QString start = query.value(idStart).toString();
+       QString finish = query.value(idFinish).toString();
+       int distance = query.value(idDistance).toInt();
+       qDebug() << "Name of city: " << start << endl;
+       qDebug() << "Distance From " << finish << " : " << distance << endl << endl;
+    }
+}
+
+void DbManager::eraseData(){
+    QSqlQuery query;
+    query.prepare("DELETE FROM cities2");
+    query.exec();
 }
 
 #endif // DATABASE_H
--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "database.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void loadCityData(DbManager &);
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    DbManager test("/home/f/projects/CS1D-vacation-project/testdb");
+    DbManager test("C:/Users/bobby/Desktop/CS1D-vacation-project/testdb");
+    //test.addCity("Paris", 100);
+   // test.printCities();
+    //cout << "deleting Data.." << endl;
+   // test.eraseData();
+    test.printCities();
+    //loadCityData(test);
 
     return a.exec();
+}
+
+void loadCityData(DbManager & db){
+
+    ifstream input;
+    input.open("C:/Users/bobby/Desktop/CS1D-vacation-project/data.csv");
+    string startCity;
+    string endCity;
+    string distance;
+    string dummyLine;
+
+    if(input.is_open()){
+        getline(input, dummyLine);
+        while(!input.eof()){
+           getline(input, startCity , ',');
+           getline(input, endCity , ',');
+           getline(input, distance , ',');
+           db.addCity(QString::fromStdString(startCity),QString::fromStdString(endCity), atof(distance.c_str()));
+        }
+    }
+    else{
+        std::cout << "Could not open data.csv" << endl;
+    }
 }
