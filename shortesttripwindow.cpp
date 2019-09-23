bf704cf9ef579a40cb9b986e0653438d836451af
Kersplato <49539722+Kersplato@users.noreply.github.com>
2019-09-23 12:52:26 -0700

    Add files via upload

--- shortesttripwindow.cpp
+++ shortesttripwindow.cpp
@@ -10,6 +10,7 @@ ShortestTripWindow::ShortestTripWindow(QWidget *parent) :
 
     QPixmap pix("C:/Users/m3isa/OneDrive/Documents/CS1D Projects/map of europe.jpg");
     ui->label_pic->setPixmap(pix.scaled(960, 622, Qt::KeepAspectRatio));
+
 }
 
 ShortestTripWindow::~ShortestTripWindow()
@@ -17,7 +18,28 @@ ShortestTripWindow::~ShortestTripWindow()
     delete ui;
 }
 
-void ShortestTripWindow::on_confirmTrip_clicked()
+void ShortestTripWindow::on_showTrip_clicked()
 {
+<<<<<<< Updated upstream
 
+=======
+    ui->orderedCityList->clear();
+
+    qDebug() << ui->spinBox1->value();
+    ui->spinBox1->setRange(1,11);
+
+    std::list<QString> * orderedCities = new std::list<QString>;
+    findRouteFastest(orderedCities, ui->spinBox1->value());
+
+    std::list<QString>::iterator it;
+
+    qDebug() << "START ORDERED CITIES\n";
+    for(it = orderedCities->begin();it != orderedCities->end(); it++)
+    {
+        qDebug() << *it << " ";
+
+        QString start = *it;
+        ui->orderedCityList->addItem(start);
+    }
+>>>>>>> Stashed changes
 }
--- shortesttripwindow.h
+++ shortesttripwindow.h
@@ -16,7 +16,7 @@ public:
     ~ShortestTripWindow();
 
 private slots:
-    void on_confirmTrip_clicked();
+    void on_showTrip_clicked();
 
 private:
     Ui::ShortestTripWindow *ui;
